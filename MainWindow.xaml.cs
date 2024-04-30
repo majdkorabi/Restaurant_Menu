86829a6c4c87b6874a0c8624de02de5722d02994
Majd Korabi <126183854+majdkorabi@users.noreply.github.com>
2024-04-30 23:33:40 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Menu
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenWindow(object sender, RoutedEventArgs e)
+        {
+            MenuWindow mw = new MenuWindow();
+            mw.Show();
+
+            // Close the MainWindow
+            this.Close();
+        }
+    }
+}
---
+++ MenuWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace Menu
+{
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void img_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image img = (Image)sender;
+            imgFullScale.Source = img.Source;
+            popFullScale.PlacementTarget = img;
+            popFullScale.Width = 150;
+            popFullScale.Height = 150;
+            popFullScale.IsOpen = true;
+        }
+        private void img_MouseLeave(object sender, MouseEventArgs e)
+        {
+            popFullScale.IsOpen = false;
+        }
+    }
+}
